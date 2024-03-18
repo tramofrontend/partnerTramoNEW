cca6fe536b27368fba33fa1a2d1c9013c967cd88
Tramoreact <dev.react@tramo.in>
2024-03-18 19:29:47 +0530

    update code

--- src/sections/FundManagement/MyFundRequest.tsx
+++ src/sections/FundManagement/MyFundRequest.tsx
@@ -13,18 +13,32 @@ import {
   Button,
   Typography,
   Pagination,
+  MenuItem,
 } from "@mui/material";
 
 import { Helmet } from "react-helmet-async";
-
+import * as Yup from "yup";
 import { useSnackbar } from "notistack";
 
 import { Api } from "src/webservices";
-import { fDateTime } from "src/utils/formatTime";
+import { fDateFormatForApi, fDateTime } from "src/utils/formatTime";
 import ApiDataLoading from "src/components/customFunctions/ApiDataLoading";
 import Label from "src/components/label/Label";
 import { sentenceCase } from "change-case";
 import CustomPagination from "src/components/customFunctions/CustomPagination";
+import { useForm } from "react-hook-form";
+import FormProvider from "src/components/hook-form/FormProvider";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useDateRangePicker } from "src/components/date-range-picker";
+import FileFilterButton from "../MyTransaction/FileFilterButton";
+import Iconify from "src/components/iconify";
+import { DateRangePicker } from "@mui/lab";
+import { RHFSelect, RHFTextField } from "src/components/hook-form";
+import Scrollbar from "src/components/scrollbar";
+import { TableHeadCustom } from "src/components/table";
+import { fIndianCurrency } from "src/utils/formatNumber";
+import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 // ----------------------------------------------------------------------
 
 export default function (props: any) {
@@ -42,6 +56,7 @@ export default function (props: any) {
   const tableLabels = [
     { id: "Date", label: "Date & Time" },
     { id: "amount", label: "Amount" },
+    { id: "modeType", label: "ModeType" },
     { id: "Charge", label: "Charge" },
     { id: "Commission", label: "Commission" },
     { id: " deposit_type", label: " Deposit Type" },
@@ -49,6 +64,63 @@ export default function (props: any) {
     { id: " branch", label: " Branch" },
     { id: " status", label: " Status" },
   ];
+
+  type FormValuesProps = {
+    transactionType: string;
+    phoneNumber: string;
+    utrNumber: string;
+    amount: string;
+    status: string;
+    Paymentmode: string;
+    request_type: string;
+    fundRequestId: string;
+    startDate: Date | null;
+    endDate: Date | null;
+  };
+
+  const FilterSchema = Yup.object().shape({
+    UserName: Yup.string(),
+  });
+
+  const defaultValues = {
+    phoneNumber: "",
+    amount: "",
+    Paymentmode: "",
+    status: "",
+    request_type: "",
+    fundRequestId: "",
+    startDate: null,
+    endDate: null,
+  };
+  const methods = useForm<FormValuesProps>({
+    resolver: yupResolver(FilterSchema),
+    defaultValues,
+  });
+
+  const {
+    reset,
+    setError,
+    setValue,
+    watch,
+    getValues,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = methods;
+
+  const {
+    startDate,
+    endDate,
+    onChangeStartDate,
+    onChangeEndDate,
+    open: openPicker,
+    onOpen: onOpenPicker,
+    onClose: onClosePicker,
+    isSelected: isSelectedValuePicker,
+    isError,
+    shortLabel,
+  } = useDateRangePicker(null, null);
+
+ 
 
   useEffect(() => {
     getFundReq();
@@ -112,135 +184,252 @@ export default function (props: any) {
     );
   };
 
+
+ 
+
+  const SearchData = (data: FormValuesProps) => {
+    setSdata([]);
+    let token = localStorage.getItem("token");
+    let body = {
+      pageInitData: {
+        pageSize: 20,
+        currentPage: currentPage,
+      },
+      bankName: "",
+      bankId: "",
+      status: data.status,
+      modeName: data.Paymentmode,
+      mobileNumber: data.phoneNumber,
+      amount: data.amount,
+      startDate: fDateFormatForApi(getValues("startDate")),
+      endDate: fDateFormatForApi(getValues("endDate")),
+      type: "",
+    };
+    Api(`agent/fundManagement/getRaisedRequests`, "POST", body, token).then(
+      (Response: any) => {
+        console.log("======Transaction==response=====>" + Response);
+        if (Response.status == 200) {
+          if (Response.data.code == 200) {
+            enqueueSnackbar(Response.data.message);
+            setPageCount(Response.data.count);
+            setSdata(Response.data.data);
+          } else {
+            console.log("======getRaisedRequests=======>" + Response);
+            enqueueSnackbar(Response.data.message);
+          }
+          setIsLoading(false);
+        } else {
+          setIsLoading(false);
+        }
+      }
+    );
+  };
+  const handdleClear = () => {
+    getFundReq();
+    onChangeEndDate(null);
+    onChangeStartDate(null);
+    reset(defaultValues);
+  };
+
   return (
     <>
       <Helmet>
         <title> Transactions |{process.env.React_APP_COMPANYNAME}</title>
       </Helmet>
 
-      <Stack flexDirection={"row"} justifyContent={"end"}>
-        <Stack
-          direction={{ xs: "column", sm: "row" }}
-          spacing={2}
-          style={{ padding: "0 25px", marginBottom: "10px" }}
-        >
-          <TextField
-            id="outlined-password-input"
-            label="Search By Ref Id"
-            size="small"
-            type="text"
-            onChange={(e) => setRefId(e.target.value)}
-          />
-          <Button variant="contained" onClick={() => filterRequest(refId)}>
-            Search
-          </Button>
-        </Stack>
-      </Stack>
+      <Stack flexDirection={"row"} justifyContent={"end"}></Stack>
       {isLoading ? (
         <ApiDataLoading />
       ) : (
         <Grid item xs={16} md={12} lg={12}>
-          <Table
-            sx={{ minWidth: 720 }}
-            stickyHeader
-            size="medium"
-            aria-label="customized table"
-          >
-            <TableHead>
-              <TableRow>
-                {tableLabels.map((column: any) => (
-                  <TableCell key={column.id} align="center">
-                    {column.label}
-                  </TableCell>
-                ))}
-              </TableRow>
-            </TableHead>
+          <FormProvider methods={methods} onSubmit={handleSubmit(SearchData)}>
+            <Stack direction="row" gap={2} mt={2} mb={2}>
+              <Stack>
+                <Stack flexDirection={"row"} gap={1}>
+                  <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <DatePicker
+                      label="Start date"
+                      inputFormat="DD/MM/YYYY"
+                      value={watch("startDate")}
+                      maxDate={new Date()}
+                      onChange={(newValue: any) => setValue("startDate", newValue)}
+                      renderInput={(params: any) => (
+                        <TextField {...params} size={"small"} sx={{ width: 150 }} />
+                      )}
+                    />
+                    <DatePicker
+                      label="End date"
+                      inputFormat="DD/MM/YYYY"
+                      value={watch("endDate")}
+                      minDate={watch("startDate")}
+                      maxDate={new Date()}
+                      onChange={(newValue: any) => setValue("endDate", newValue)}
+                      renderInput={(params: any) => (
+                        <TextField {...params} size={"small"} sx={{ width: 150 }} />
+                      )}
+                    />
+                  </LocalizationProvider>
+                </Stack>
+              </Stack>
+              <RHFSelect
+                name="Paymentmode"
+                label="Mode Of Payment"
+                SelectProps={{
+                  native: false,
+                  sx: { textTransform: "capitalize" },
+                }}
+              >
+                <MenuItem value="NEFT">NEFT</MenuItem>
+                <MenuItem value="RTGS">RTGS</MenuItem>
+                <MenuItem value="IMPS">IMPS</MenuItem>
+                <MenuItem value="Cash deposit at CDM">
+                  Cash deposit at CDM
+                </MenuItem>
+                <MenuItem value="Fund Transfer">Fund Transfer</MenuItem>
+                <MenuItem value="Cash deposit at branch">
+                  Cash deposit at branch
+                </MenuItem>
+              </RHFSelect>
 
-            <TableBody>
-              {sdata.map((row: any) => (
-                <TableRow
-                  key={row._id}
-                  hover
-                  role="checkbox"
-                  tabIndex={-1}
-                  sx={{ borderBottom: "1px solid #dadada" }}
-                  // hover
-                >
-                  <TableCell>
-                    <Typography variant="body1">
-                      {fDateTime(row?.createdAt)}
-                    </Typography>
-                  </TableCell>
+              <RHFSelect
+                name="status"
+                label="Status"
+                SelectProps={{
+                  native: false,
+                  sx: { textTransform: "capitalize" },
+                }}
+              >
+                <MenuItem value="Approved">Approved</MenuItem>
+                <MenuItem value="Pending">Pending</MenuItem>
+                <MenuItem value="Rejected">Rejected</MenuItem>
+              </RHFSelect>
 
-                  <TableCell>
-                    <Typography variant="body1">Rs. {row?.amount}</Typography>
-                  </TableCell>
+              {
+                <RHFTextField
+                  name="phoneNumber"
+                  label=" Mobile"
+                  placeholder="Mobile"
+                  size="small"
+                />
+              }
+              <RHFTextField
+                name="amount"
+                label="amount"
+                placeholder="amount"
+                size="small"
+              />
 
-                  <TableCell>
-                    <Typography variant="body1" textAlign={"center"}>
-                      {!isNaN(row?.Charge) ? "Rs. " + row?.Charge : "-"}
-                    </Typography>
-                  </TableCell>
+              <Button variant="contained" type="submit">
+                Search
+              </Button>
+              <Button variant="contained" onClick={handdleClear}>
+                Clear
+              </Button>
+            </Stack>
+          </FormProvider>
 
-                  <TableCell>
-                    <Typography variant="body1" textAlign={"center"}>
-                      {!isNaN(row?.Commission)
-                        ? "Rs. " + parseFloat(row?.Commission).toFixed(2)
-                        : "-"}
-                    </Typography>
-                  </TableCell>
+          <Scrollbar sx={{ maxHeight: window.innerHeight - 160 }}>
+            <Table
+              sx={{ minWidth: 720 }}
+              aria-label="customized table"
+              stickyHeader
+            >
+              <TableHeadCustom headLabel={tableLabels} />
 
-                  <TableCell>
-                    <Typography variant="body1">{row?.deposit_type}</Typography>
-                  </TableCell>
-
-                  <TableCell>
-                    <Typography variant="body1">
-                      {row?.transactional_details?.mobile}
-                    </Typography>
-                  </TableCell>
-
-                  <TableCell>
-                    <Typography variant="body1">
-                      {row?.transactional_details?.branch}
-                    </Typography>
-                  </TableCell>
-
-                  <TableCell
-                    sx={{
-                      textTransform: "lowercase",
-                      fontWeight: 600,
-                      textAlign: "center",
-                    }}
+              <TableBody>
+                {sdata.map((row: any) => (
+                  <TableRow
+                    key={row._id}
+                    role="checkbox"
+                    tabIndex={-1}
+                    sx={{ borderBottom: "1px solid #dadada" }}
                   >
-                    <Label
-                      variant="soft"
-                      color={
-                        (row.status.toLowerCase() === "failed" && "error") ||
-                        ((row.status.toLowerCase() === "pending" ||
-                          row.status.toLowerCase() === "in_process") &&
-                          "warning") ||
-                        "success"
-                      }
-                      sx={{ textTransform: "capitalize" }}
+                    <TableCell>
+                      <Typography variant="body1">
+                        {fDateTime(row?.createdAt)}
+                      </Typography>
+                    </TableCell>
+
+                    <TableCell>
+                      <Typography variant="body1">Rs. {row?.amount}</Typography>
+                    </TableCell>
+                    <TableCell>
+                      <Typography variant="body1">
+                        {row?.modeId?.transfer_mode_name}
+                      </Typography>
+                    </TableCell>
+
+                    <TableCell>
+                      <Typography variant="body1" textAlign={"center"}>
+                        {!isNaN(row?.Charge) ? "Rs. " + row?.Charge : "-"}
+                      </Typography>
+                    </TableCell>
+
+                    <TableCell>
+                      <Typography variant="body1" textAlign={"center"}>
+                        {!isNaN(row?.Commission)
+                          ? "Rs. " + fIndianCurrency(row?.Commission || "0")
+                          : "-"}
+                      </Typography>
+                    </TableCell>
+
+                    <TableCell>
+                      <Typography variant="body1">
+                        {row?.deposit_type}
+                      </Typography>
+                    </TableCell>
+
+                    <TableCell>
+                      <Typography variant="body1">
+                        {row?.transactional_details?.mobile}
+                      </Typography>
+                    </TableCell>
+
+                    <TableCell>
+                      <Typography variant="body1">
+                        {row?.transactional_details?.branch}
+                      </Typography>
+                    </TableCell>
+
+                    <TableCell
+                      sx={{
+                        textTransform: "lowercase",
+                        fontWeight: 600,
+                        textAlign: "center",
+                      }}
                     >
-                      {row.status.toLowerCase()
-                        ? sentenceCase(row.status.toLowerCase())
-                        : ""}
-                    </Label>
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
+                      <Label
+                        variant="soft"
+                        color={
+                          (row.status.toLowerCase() === "failed" && "error") ||
+                          ((row.status.toLowerCase() === "pending" ||
+                            row.status.toLowerCase() === "in_process") &&
+                            "warning") ||
+                          "success"
+                        }
+                        sx={{ textTransform: "capitalize" }}
+                      >
+                        {row.status.toLowerCase()
+                          ? sentenceCase(row.status.toLowerCase())
+                          : ""}
+                      </Label>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </Scrollbar>
+
           <CustomPagination
-            pageSize={pageSize}
-            onChange={(event: React.ChangeEvent<unknown>, value: number) => {
-              setCurrentPage(value);
-            }}
-            page={currentPage}
-            Count={pageCount}
-          />
+               pageSize={pageSize}
+               onChange={(event: React.ChangeEvent<unknown>, value: number) => {
+                 setCurrentPage(value);
+               }}
+               page={currentPage}
+               Count={pageCount}
+             />
+
+          {/* <CustomPagination /> */}
         </Grid>
       )}
     </>
