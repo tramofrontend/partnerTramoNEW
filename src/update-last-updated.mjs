5307e391ad4902eceeee612d3d2dffc75b1d0901
FrontDevMain <sharad.kumar@tramo.in>
2024-04-09 19:12:31 +0530

    Update update-last-updated.mjs

--- src/update-last-updated.mjs
+++ src/update-last-updated.mjs
@@ -1,7 +1,7 @@
-import { fileURLToPath } from "url";
-import { dirname } from "path";
-import * as fs from "fs";
-import { format } from "date-fns";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import * as fs from 'fs';
+import { format } from 'date-fns';
 
 // import statements
 const __filename = fileURLToPath(import.meta.url);
@@ -11,11 +11,27 @@ const packagePath = `${__dirname}/../package.json`;
 
 // Function to update lastUpdated field in package.json
 const updateLastUpdated = () => {
-  const currentDate = format(new Date(), "dd MMM yyyy, pp"); // Get current date and time in IST
-  const packageJson = JSON.parse(fs.readFileSync(packagePath, "utf8")); // Read package.json
+  var currentTime = new Date();
+  var currentOffset = currentTime.getTimezoneOffset();
+  var ISTOffset = 330; // IST offset UTC +5:30
+  var ISTTime = new Date(currentTime.getTime() + (ISTOffset + currentOffset) * 60000);
+
+  // ISTTime now represents the time in IST coordinates
+
+  var dateIST = ISTTime.getDate();
+  var monthIST = ISTTime.getMonth();
+  var yearIST = ISTTime.getFullYear();
+
+  var hoursIST = ISTTime.getHours();
+  var minutesIST = ISTTime.getMinutes();
+  var secondsIST = ISTTime.getSeconds();
+
+  const currentDate =
+    `${dateIST}/${monthIST}/${yearIST}` + ' ' + hoursIST + ':' + minutesIST + ':' + secondsIST; // Get current date and time in IST
+  const packageJson = JSON.parse(fs.readFileSync(packagePath, 'utf8')); // Read package.json
   packageJson.lastUpdated = currentDate; // Update lastUpdated field
   fs.writeFileSync(packagePath, JSON.stringify(packageJson, null, 2)); // Write updated package.json
-  console.log("Last updated time:", currentDate); // Log updated time
+  console.log('Last updated time:', currentDate); // Log updated time
 };
 
 updateLastUpdated(); // Call the function to update lastUpdated field
