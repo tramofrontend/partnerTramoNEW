a52476e1ea5dbe5d9bec6855c141d18c2722849b
Tramoreact <163147385+Tramoreact@users.noreply.github.com>
2024-07-11 17:07:46 +0530

    Add DMT1 Url

--- src/components/Skeletons/CallBackSkeleton.tsx
+++ src/components/Skeletons/CallBackSkeleton.tsx
@@ -27,6 +27,13 @@ export const CallBackSkeleton = () => {
           <Skeleton variant="text" sx={{ fontSize: '1rem' }} width={70} height={60} />
         </Stack>
       </Stack>
+      <Stack justifyContent={'space-between'}>
+        <Skeleton variant="text" sx={{ fontSize: '1rem' }} width={600} height={40} />
+        <Stack flexDirection={'row'} gap={3} marginTop={3}>
+          <Skeleton variant="text" sx={{ fontSize: '1rem' }} width={150} height={60} />
+          <Skeleton variant="text" sx={{ fontSize: '1rem' }} width={70} height={60} />
+        </Stack>
+      </Stack>
       </Stack>
       <Stack>
       <Stack justifyContent={'space-between'}>
